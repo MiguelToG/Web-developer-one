577d1e198373e883ff791f6cfc78506bd32a5180
Miguel Flores <rmiguel.0.0.77@gmail.com>
2017-10-15 00:22:48 -0500

    Proyects
    Todo List and Patatap Clone finished

--- Todo proyect/assets/js/todos.js
+++ Todo proyect/assets/js/todos.js
@@ -1,15 +1,28 @@
 
 //check off speficif ToDos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //Click on X to delete ToDo
 
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500,function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+$("input[type='text']").keypress(function (event) {
+    if(event.which === 13){
+        //Store the text in a new variable
+        var TodoText =  $(this).val();
+        //Clear the input
+        $(this).val("");
+        //create a new li and add it to the ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + TodoText +"</li>");
+    }
+})
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
 });
